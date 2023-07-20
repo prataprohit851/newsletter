65f1b1353f353c75be7f516505119427e8539bce
prataprohit851 <prataprohit851@gmail.com>
2023-07-20 18:12:35 +0530

    api edits

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const client = require("@mailchimp/mailchimp_marketing");
-
+const fs = require('fs')
 
 const app = express();
 
@@ -15,9 +15,12 @@ app.post("/", function(req, res){
     console.log(fName, lName, email);
 
 
+    var apiKeyy = fs.readFileSync('key.txt','utf8')
 
+    console.log("2");
+    
     client.setConfig({
-        apiKey: "fe53a8f9739269bf7409239ac7466b74-us21",
+        apiKey: apiKeyy,
         server: "us21",
       });
       
@@ -32,6 +35,8 @@ app.post("/", function(req, res){
               }
           }],
         });
+
+
         if(response.errors[0] == undefined){
             res.sendFile(__dirname + "/success.html");
         } else if(response.errors[0].error === email + ' is already a list member, do you want to update? please provide update_existing:true in the request body'){
@@ -48,18 +53,9 @@ app.post("/", function(req, res){
 });
 
 app.get("/", function(req, res){
-    // res.send("Working");
     res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000), function(){
     console.log("Server is running");
 };
-
-// API key
-// fe53a8f9739269bf7409239ac7466b74-us21
-
-// List ID
-// a063f71f1d
-
-
